80428974eeb4c8613aadc689f788938a08982e22
mrlicmi <7086110+mrlicmi@users.noreply.github.com>
2024-07-01 12:25:43 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Hello, world!"
+echo "This is a simple script that prints messages to the console."
+echo "Current date and time: $(date)"
+echo "Current working directory: $(pwd)"
+echo "List of files in the current directory:"
+ls -l
