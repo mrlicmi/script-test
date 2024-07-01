e8a52e53aa93c0532186e3bac995526637bffb63
mrlicmi <7086110+mrlicmi@users.noreply.github.com>
2024-07-01 14:34:42 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,4 +5,5 @@ echo "This is a simple script that prints messages to the console."
 echo "Current date and time: $(date)"
 echo "Current working directory: $(pwd)"
 echo "List of files in the current directory:"
+spin -v
 ls -l
